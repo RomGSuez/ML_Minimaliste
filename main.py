a9abdf68de844936db3d269895c49fea37a89391
RomGSuez <romain.girodet.espagne@gmail.com>
2022-06-30 17:18:33 +0200

    Correction for 1 presentation

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 from Interface import Interface
 from ML.ML_trainAlgo import Trainer
 
-
+import joblib
 
 
 
 if __name__ == '__main__':
-    Trainer.initialize_classifier()
+    # Trainer.initialize_classifier()
 
 
     #TEST
-    tree=Trainer.load_classifier()
+    
+    # tree=Trainer.load_classifier()
+    tree=joblib.load('ML\saved_clf_tree.pkl')
     print(tree)
 
     interface=Interface()
@@ -18,4 +20,5 @@ if __name__ == '__main__':
     result=interface.predict_input()
 
     with open('result.txt', 'w') as f:
+        f.write(str("species\n"))
         f.write(str(result))
