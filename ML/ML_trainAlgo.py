f88388b2893b33e4c3898216231299aceba57748
RomGSuez <romain.girodet.espagne@gmail.com>
2022-08-01 16:20:25 +0200

    add txt

--- ML/ML_trainAlgo.py
+++ ML/ML_trainAlgo.py
@@ -9,19 +9,20 @@ from sklearn.tree import DecisionTreeClassifier
 import joblib
 
 class Trainer:
-       
+
+    # Load dataset
+    iris = load_iris()
+        
+    X = iris.data
+    y = iris.target
+         
+    # Split dataset into train and test
+    X_train, X_test, y_train, y_test = \
+        train_test_split(X, y, test_size = 0.3,
+                            random_state = 2018)
+    
     def initialize_classifier():
-        # Load dataset
-        iris = load_iris()
-         
-        X = iris.data
-        y = iris.target
-         
-        # Split dataset into train and test
-        X_train, X_test, y_train, y_test = \
-            train_test_split(X, y, test_size = 0.3,
-                                random_state = 2018)
-         
+        
         # Create DecisionTreeClassifier model
         tree = DecisionTreeClassifier(max_depth = 10).fit(X_train, y_train)
 
@@ -35,6 +36,11 @@ class Trainer:
         return  joblib.load('ML/saved_clf_tree.pkl')
 
     
+    def initialize_regressor():
+        pass
+
+    def load_classifier(): 
+        return  joblib.load('ML/saved_regressor_tree.pkl')
     
 
 
