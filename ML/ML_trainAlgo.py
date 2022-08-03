1b9c8bb53e017ee7e9e0df19ab3002605bcf53a7
RomGSuez <romain.girodet.espagne@gmail.com>
2022-08-03 16:11:18 +0200

    PB with pkl

--- ML/ML_trainAlgo.py
+++ ML/ML_trainAlgo.py
@@ -11,6 +11,8 @@ import joblib
 
 class Trainer:
 
+    
+    
     # Load dataset
     iris = load_iris()
         
@@ -21,25 +23,17 @@ class Trainer:
     X_train, X_test, y_train, y_test = \
         train_test_split(X, y, test_size = 0.3,
                             random_state = 2018)
-
-    def initialize_classifier():
         
+    def train_classifier(): 
         # Create DecisionTreeClassifier model
-        tree = DecisionTreeClassifier(max_depth = 10).fit(X_train, y_train)
-
-        # Save the model as a pickle in a file
-        joblib.dump(tree, 'saved_clf_tree.pkl')
-     
+        return  DecisionTreeClassifier(max_depth = 10).fit(X_train, y_train)
+            
     def load_classifier(): 
         return  joblib.load('ML/saved_clf_tree.pkl')
-
-   
-    def initialize_regressor():
+  
+    def train_regressor():
         # Create DecisionTreeClassifier model
-        tree = ExtraTreesRegressor(max_depth = 10).fit(X_train, y_train)
-
-        # Save the model as a pickle in a file
-        joblib.dump(tree, 'saved_regressor_tree.pkl')
+        return ExtraTreesRegressor(max_depth = 10).fit(X_train, y_train)     
 
     def load_regressor(): 
         return  joblib.load('ML/saved_regressor_tree.pkl')
--- main.py
+++ main.py
@@ -8,17 +8,26 @@ import joblib
 if __name__ == '__main__':
   
     interface=Interface()
- 
+    trainer=Trainer()
+    
+    joblib.dump(trainer.train_classifier, 'ML/saved_clf_tree.pkl')
+    joblib.dump(trainer.train_regressor, 'ML/saved_regressor_tree.pkl')
+    
+    
     # I- Classifier
     result_classifier=interface.predict_input_classifier()
     
-    with open('result_classifier.txt', 'w') as f:
+    with open('Classifier/result_classifier.txt', 'w') as f:
         f.write(str("species\n"))
         f.write(str(result_classifier))
+        
+        print("result_classifier",result_classifier)
     
     # II- Regressor
     result_regressor=interface.predict_input_regressor()
 
-    with open('result_regressor.txt', 'w') as f:
-        f.write(str("species\n"))
+    with open('Regressor/result_regressor.txt', 'w') as f:
+        # f.write(str("species\n"))
         f.write(str(result_regressor))
+        
+        print("result_regressor",result_regressor)
