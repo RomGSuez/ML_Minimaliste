c65f482a558373787fd249a23e6ef93989145a63
RomGSuez <romain.girodet.espagne@gmail.com>
2022-08-11 11:31:59 +0200

    Classifier and regressor

--- ML/ML_trainAlgo.py
+++ ML/ML_trainAlgo.py
@@ -8,35 +8,67 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import ExtraTreesRegressor
 
 import joblib
+import pickle
+
 
 class Trainer:
-
-    
-    
-    # Load dataset
-    iris = load_iris()
+  
         
-    X = iris.data
-    y = iris.target
-         
-    # Split dataset into train and test
-    X_train, X_test, y_train, y_test = \
-        train_test_split(X, y, test_size = 0.3,
-                            random_state = 2018)
-        
-    def train_classifier(): 
+    def train_classifier(self):
+        # Load dataset
+        iris = load_iris()
+            
+        X = iris.data
+        y = iris.target
+             
+        # Split dataset into train and test
+        X_train, X_test, y_train, y_test = \
+            train_test_split(X, y, test_size = 0.3,
+                                random_state = 2018)
+            
         # Create DecisionTreeClassifier model
-        return  DecisionTreeClassifier(max_depth = 10).fit(X_train, y_train)
+        tree =DecisionTreeClassifier(max_depth = 10).fit(X_train, y_train)
+        pickle.dump(tree, open("ML/saved_clf_tree.pkl", "wb"))
+        
             
     def load_classifier(): 
+        # print("load")
         return  joblib.load('ML/saved_clf_tree.pkl')
   
-    def train_regressor():
-        # Create DecisionTreeClassifier model
-        return ExtraTreesRegressor(max_depth = 10).fit(X_train, y_train)     
+    
+  
+    
+    def train_regressor(self):
+        # Load dataset
+        iris = load_iris()
+        X = iris.data
+        
+        #Create new X df without the target variable (petal width)
+        # Create new X df with the target variable (petal width)
+        new_X=[]
+        new_y=[]
+         
+        for arr in X:
+            new_X.append(arr[:-1])
+            new_y.append(arr[-1:])
+          
+        
+        # Split dataset into train and test
+        X_train, X_test, y_train, y_test = \
+        train_test_split(new_X, new_y, test_size = 0.3,
+                            random_state = 2018)
+        
+        
+        # Create ExtraTreesRegressor model
+        tree =ExtraTreesRegressor(max_depth = 10).fit(X_train, y_train)
+        pickle.dump(tree, open("ML/saved_regressor_tree.pkl", "wb"))
+        
+        
+        
+            
 
     def load_regressor(): 
+        # print("load")
         return  joblib.load('ML/saved_regressor_tree.pkl')
     
-
 
--- main.py
+++ main.py
@@ -1,24 +1,27 @@
 from Interface import Interface
 from ML.ML_trainAlgo import Trainer
 
-import joblib
-
+# import joblib
+import pickle
 
 
 if __name__ == '__main__':
-  
+    
+    
+    # INITIALIZE Classes 
     interface=Interface()
     trainer=Trainer()
     
-    joblib.dump(trainer.train_classifier, 'ML/saved_clf_tree.pkl')
-    joblib.dump(trainer.train_regressor, 'ML/saved_regressor_tree.pkl')
     
+    trainer.train_classifier()
+    trainer.train_regressor()
+  
     
     # I- Classifier
     result_classifier=interface.predict_input_classifier()
     
     with open('Classifier/result_classifier.txt', 'w') as f:
-        f.write(str("species\n"))
+        f.write(str("species:\n"))
         f.write(str(result_classifier))
         
         print("result_classifier",result_classifier)
@@ -27,7 +30,7 @@ if __name__ == '__main__':
     result_regressor=interface.predict_input_regressor()
 
     with open('Regressor/result_regressor.txt', 'w') as f:
-        # f.write(str("species\n"))
+        f.write(str("petal width:\n"))
         f.write(str(result_regressor))
         
         print("result_regressor",result_regressor)
