8fc0efbe6305d13ab98224d4b1a7fb1877c10412
RomGSuez <romain.girodet.espagne@gmail.com>
2022-08-11 15:48:13 +0200

    modify input clf y rgs

--- Interface.py
+++ Interface.py
@@ -7,7 +7,7 @@ from ML.ML_trainAlgo import Trainer
 class Interface():
     
     def read_input_classifier(self):
-        return pd.read_csv("Classifier/input_classifier.txt",delimiter=";",header=3)
+        return pd.read_csv("Classifier/input_classifier.txt",delimiter=";",header=5)
     
     def predict_input_classifier(self):
         
@@ -15,7 +15,7 @@ class Interface():
         return tree.predict(self.read_input_classifier())
     
     def read_input_regressor(self):
-        return pd.read_csv("Regressor/input_regressor.txt",delimiter=";",header=3)
+        return pd.read_csv("Regressor/input_regressor.txt",delimiter=";",header=5)
     
     def predict_input_regressor(self):
         
